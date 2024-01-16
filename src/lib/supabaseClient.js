4bbb4cb91accc288baa8b7351df0d2b002c2d3f3
nomideusz <b.dymet@gmail.com>
2024-01-16 04:11:42 +0100

    Remove vercel.json

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+	'https://kbvgjpyrihzmqbmnvjdz.supabase.co',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtidmdqcHlyaWh6bXFibW52amR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUyOTAwMDEsImV4cCI6MjAyMDg2NjAwMX0.6Zy74XV_-O8PkTqz5wqyhQBfMu-GjEDSEG3gP1hwCYY'
+);
