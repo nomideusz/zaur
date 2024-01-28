aec9c940a5f8f7816d553f15efc8b2191f8d864e
nomideusz <b.dymet@gmail.com>
2024-01-28 14:05:46 +0100

    Env var test

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_KEY } from '$env/static/public';
+let supabaseKey;
+
+if (process.env.NODE_ENV === 'production') {
+    // For production
+    supabaseKey = process.env.PUBLIC_SUPABASE_KEY;
+} else {
+    // For development
+    supabaseKey = PUBLIC_SUPABASE_KEY;
+}
 const supabaseUrl = 'http://188.68.236.198:8000'
-const supabaseKey = PUBLIC_SUPABASE_KEY
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
