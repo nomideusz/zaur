501e61b4cfa74a530ca4ff37a07ddcf83af5877b
nomideusz <b.dymet@gmail.com>
2024-01-28 20:45:02 +0100

    Change env var

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_KEY } from '$env/static/public';
-const supabaseUrl = 'http://188.68.236.198:8000'
-export const supabase = createClient(supabaseUrl, PUBLIC_SUPABASE_KEY)
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY)
