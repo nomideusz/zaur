253507dd1eb6a0aaabc21cc1629f3f7d5208551d
nomideusz <b.dymet@gmail.com>
2024-02-03 20:23:54 +0100

    Add counters animation

--- src/routes/sections/Stats.svelte
+++ src/routes/sections/Stats.svelte
@@ -1,38 +1,40 @@
 <script lang="ts">
   import { adsCount } from '$lib/adsCountStore';
-
+  import Typewriter from 'svelte-typewriter';
+  let randomNumber =  Math.floor(Math.random() * 9999);
+  $: numberOfAds = $adsCount ? $adsCount : 0;
 </script>
-
+<Typewriter mode="scramble">
 <!-- Features -->
 <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     <!-- Grid -->
     <div class="mt-20 grid gap-6 grid-cols-2 sm:gap-12 lg:grid-cols-3 lg:gap-8">
       <!-- Stats -->
       <div>
-        <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Number of Ads</h4>
+        <h4 data-static class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Number of Ads</h4>
         <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">
-            {$adsCount}
+          {numberOfAds}
         </p>
-        <p class="mt-1 text-gray-500">unique ads without ads</p>
+        <p data-static class="mt-1 text-gray-500">unique ads without ads</p>
     </div>
       <!-- End Stats -->
   
       <!-- Stats -->
       <div>
-        <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Number of Users</h4>
-        <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">0</p>
-        <p class="mt-1 text-gray-500">and counting</p>
-      </div>
-      <!-- End Stats -->
-  
-      <!-- Stats -->
-      <div>
-        <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Number of Zaurs</h4>
+        <h4 data-static class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Number of Zaurs</h4>
         <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">1</p>
-        <p class="mt-1 text-gray-500">powerful</p>
+        <p data-static class="mt-1 text-gray-500">powerful</p>
       </div>
       <!-- End Stats -->
+            <!-- Stats -->
+            <div>
+              <h4 data-static class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">A Random Number</h4>
+              <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">{randomNumber}</p>
+              <p data-static class="mt-1 text-gray-500">between 0 and 9999</p>
+            </div>
+            <!-- End Stats -->
     </div>
     <!-- End Grid -->
   </div>
   <!-- End Features -->
+</Typewriter>
