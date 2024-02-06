157c4beeb0df3becf3236d20a840b019dbaafaaa
nomideusz <b.dymet@gmail.com>
2024-02-06 21:53:24 +0100

    Add cool animations.

--- src/routes/sections/Hero.svelte
+++ src/routes/sections/Hero.svelte
@@ -1,14 +1,38 @@
 <script lang="ts">
   import logo from '$lib/img/zaur.png?enhanced&w=400';
   import Butt from '$lib/components/ui/butt/butt.svelte';
+  import { blur } from 'svelte/transition';
+  import { circIn, circOut } from 'svelte/easing';
+  import { Gooey, timerStore } from 'svelte-ux';
+  import { cls } from 'svelte-ux';
+  const indexTimer = timerStore({
+    initial: 0,
+    delay: 4000,
+    onTick: (value) => (value ?? 0) + 1,
+  });
 </script>
-<main id="content" class="flex items-center justify-center w-full mx-auto">
+
+<main id="content" class="flex items-center justify-center w-full text-center mx-auto">
+
+
+
   <div class="text-center px-4 sm:px-6 lg:px-8 relative z-10">
-    <h1 class="mb-4 sm:mb-8 text-4xl text-white sm:text-6xl">
-      This <span class=" font-bold bg-clip-text bg-gradient-to-tr from-blue-600 to-purple-400 text-transparent">is</span>
-  </h1>
+
   <enhanced:img class="mb-4 sm:mb-8" src={logo} alt="Zaur" />
 
+  <Gooey blur={5} alphaPixel={255} alphaShift={-144}>
+    {@const words = ["This", "is", "Zaur"]}
+    <div class="text-8xl font-bold text-blue-200 h-24">
+      {#key $indexTimer}
+        <span class="absolute left-0 right-0"
+          in:blur={{ amount: "10px", duration: 2000, easing: circOut }}
+          out:blur={{ amount: "100px", duration: 2000, easing: circIn }}
+        >
+          {words[($indexTimer ?? 0) % words.length]}
+        </span>
+      {/key}
+    </div>
+  </Gooey>
   <!-- Buttons -->
   <div class="mt-8 flex justify-center">
       <Butt href="/sales">
--- src/routes/sections/Stats.svelte
+++ src/routes/sections/Stats.svelte
@@ -1,19 +1,29 @@
 <script lang="ts">
   import { adsCount } from '$lib/adsCountStore';
-  import Typewriter from 'svelte-typewriter';
+  import Button from '$lib/components/ui/button/button.svelte';
+	import { onMount } from 'svelte';
+  import { SpringValue, format } from 'svelte-ux';
   let randomNumber =  Math.floor(Math.random() * 9999);
-  $: numberOfAds = $adsCount ? $adsCount : 0;
+  let numberOfAds : number = 0;
+  let numberOfZaurs : number = 0;
+  let numberOfRandom : number = 0;
+  onMount(async () => {
+    numberOfAds += $adsCount;
+    numberOfZaurs += 1;
+    numberOfRandom += randomNumber;
+	});
 </script>
-<Typewriter mode="scramble">
+
+
 <!-- Features -->
-<div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto">
+<div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto text-center">
     <!-- Grid -->
     <div class="mt-20 grid gap-6 grid-cols-2 sm:gap-12 lg:grid-cols-3 lg:gap-8">
       <!-- Stats -->
       <div>
         <h4 data-static class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Number of Ads</h4>
-        <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold bg-clip-text bg-gradient-to-tr from-blue-600 to-purple-400 text-transparent">
-          {numberOfAds}
+        <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-200">
+          <SpringValue value={numberOfAds} format="integer" options={{ stiffness: 0.01, damping: 0.25 }}  />
         </p>
         <p data-static class="mt-1 text-gray-500">unique ads without ads</p>
     </div>
@@ -22,14 +32,18 @@
       <!-- Stats -->
       <div>
         <h4 data-static class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Number of Zaurs</h4>
-        <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold bg-clip-text bg-gradient-to-tr from-blue-600 to-purple-400 text-transparent">1</p>
+        <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-200">
+          <SpringValue value={numberOfZaurs} format="integer" options={{ stiffness: 0.01, damping: 0.25 }}  />
+        </p>
         <p data-static class="mt-1 text-gray-500">powerful</p>
       </div>
       <!-- End Stats -->
             <!-- Stats -->
             <div>
               <h4 data-static class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">A Random Number</h4>
-              <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold bg-clip-text bg-gradient-to-tr from-blue-600 to-purple-400 text-transparent">{randomNumber}</p>
+              <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-200">
+                <SpringValue value={numberOfRandom} format="integer" options={{ stiffness: 0.01, damping: 0.25 }}  />
+              </p>
               <p data-static class="mt-1 text-gray-500">between 0 and 9999</p>
             </div>
             <!-- End Stats -->
@@ -37,4 +51,3 @@
     <!-- End Grid -->
   </div>
   <!-- End Features -->
-</Typewriter>
