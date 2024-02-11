46b1ad173614a3966254ad7a3d20adcf3ed7be49
nomideusz <b.dymet@gmail.com>
2024-02-11 22:57:36 +0100

    Trigger test

--- src/jobs/scrape-ads.ts
+++ src/jobs/scrape-ads.ts
@@ -7,29 +7,30 @@ export const scrapeAds = client.defineJob({
   name: "Scrape Ads and Save to Database",
   version: "0.0.1",
   trigger: invokeTrigger(),
+
+
   run: async (payload, io, ctx) => {
+    const endpointUrl = `http://109.123.250.191:5050/fetch-ads/${payload.service}/${payload.type}/${payload.category}/${payload.adType}`;
+    const content = await io.backgroundFetch(
+      "fetch-content",
+      endpointUrl,
+      {
+        method: "GET",
+      },
+      {},
+      {
+        durationInMs: 60000,
+        retry: {
+          limit: 6,
+          minTimeoutInMs: 1000,
+          maxTimeoutInMs: 60000,
+          factor: 2,
+          randomize: true,
+        },
+      }
+    );
 
-    const content = await io.runTask("fetch-content", async () => {
-      return await fetchPageContent({
-        service: payload.service,
-        type: payload.type,
-        category: payload.category,
-        adType: payload.adType
-      });
-    }, { name: "Fetch Page Content" });
-
-    const ads = await io.runTask("parse-olx-ads", async () => {
-      return parseAdsOLX(content, payload.type, payload.adType);
-    }, { name: "Parse OLX Ads" });
-
-    const tableName = getTableName(payload.category);
-
-    await io.runTask("insert-ads", async () => {
-      return insertAds(ads, tableName);
-    }, { name: "Insert Ads to Database" });
-
-    return {
-      message: "Page content fetched and ads extracted successfully. Ads inserted to database."
-    };
+    return content;
   },
 });
+
