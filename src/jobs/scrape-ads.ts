ed2825bed966568cbb0a698fcb7ed1ecde7ed5a3
nomideusz <b.dymet@gmail.com>
2024-02-11 23:24:11 +0100

    Trigger schedule test

--- src/jobs/scrape-ads.ts
+++ src/jobs/scrape-ads.ts
@@ -1,36 +1,53 @@
-import { invokeTrigger } from "@trigger.dev/sdk";
+import { intervalTrigger } from "@trigger.dev/sdk";
 import { client } from "../trigger";
-import { fetchPageContent, parseAdsOLX, insertAds, getTableName } from "./scraper"; // Import funkcji
+
+const services = ["olx"]; // Przykładowa lista serwisów
+const types = ["mieszkania", "domy"]; // Przykładowe typy nieruchomości
+const categories = ["sprzedaz", "wynajem"]; // Przykładowe kategorie
+const adTypes = ["private", "business"]; // Przykładowe typy ogłoszeń
 
 export const scrapeAds = client.defineJob({
-  id: "scrape-ads",
-  name: "Scrape Ads and Save to Database",
-  version: "0.0.1",
-  trigger: invokeTrigger(),
+	id: "scrape-ads",
+	name: "Scrape Ads and Save to Database",
+	version: "0.0.1",
+	trigger: intervalTrigger({
+		seconds: 600,
+	}),
 
+	run: async (payload, io, ctx) => {
+		const serviceIndex = Math.floor(Math.random() * services.length);
+		const typeIndex = Math.floor(Math.random() * types.length);
+		const categoryIndex = Math.floor(Math.random() * categories.length);
+		const adTypeIndex = Math.floor(Math.random() * adTypes.length);
+		const dynamicPayload = {
+			service: services[serviceIndex],
+			type: types[typeIndex],
+			category: categories[categoryIndex],
+			adType: adTypes[adTypeIndex],
+		};
+		const endpointUrl = `http://109.123.250.191:5050/fetch-ads/${dynamicPayload.service}/${dynamicPayload.type}/${dynamicPayload.category}/${dynamicPayload.adType}`;
+		const content = await io.backgroundFetch(
+			"fetch-content",
+			endpointUrl,
+			{
+				method: "GET",
+			},
+			{},
+			{
+				durationInMs: 60000,
+				retry: {
+					limit: 6,
+					minTimeoutInMs: 1000,
+					maxTimeoutInMs: 60000,
+					factor: 2,
+					randomize: true,
+				},
+			}
+		);
 
-  run: async (payload, io, ctx) => {
-    const endpointUrl = `http://109.123.250.191:5050/fetch-ads/${payload.service}/${payload.type}/${payload.category}/${payload.adType}`;
-    const content = await io.backgroundFetch(
-      "fetch-content",
-      endpointUrl,
-      {
-        method: "GET",
-      },
-      {},
-      {
-        durationInMs: 60000,
-        retry: {
-          limit: 6,
-          minTimeoutInMs: 1000,
-          maxTimeoutInMs: 60000,
-          factor: 2,
-          randomize: true,
-        },
-      }
-    );
-
-    return content;
-  },
+		return {
+			message: "Content fetched successfully with random parameters.",
+			content,
+		};
+	},
 });
-
