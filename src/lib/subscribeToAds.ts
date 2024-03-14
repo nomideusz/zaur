ea494d1e7b8e32afd5f1b0482344671ad0b79fe0
unknown <b.dymet@gmail.com>
2024-03-14 22:26:35 +0100

    Add zauring toast

--- src/lib/subscribeToAds.ts
+++ src/lib/subscribeToAds.ts
@@ -1,41 +1,65 @@
 import { ads } from "$lib/adsStore";
 import { adsCount } from "$lib/adsCountStore";
 import { supabase } from "$lib/supabase";
+import { toast } from "svelte-sonner";
+
+let firstLoadComplete = false;
+let toastDisplayed = false;
+
+function showToast(message) {
+    if (!toastDisplayed) {
+        toast.info(message);
+        toastDisplayed = true;
+        setTimeout(() => {
+            toastDisplayed = false;
+        }, 10000);
+    }
+}
 
 export async function subscribeToAds() {
-	const { data: adsSell } = await supabase
-		.from("ads_sell")
-		.select("*", { count: "exact" });
-	const { data: adsRent } = await supabase
-		.from("ads_rent")
-		.select("*", { count: "exact" });
-	adsCount.set(adsSell.length + adsRent.length);
+    const { data: adsSell } = await supabase
+        .from("ads_sell")
+        .select("*", { count: "exact" });
+    const { data: adsRent } = await supabase
+        .from("ads_rent")
+        .select("*", { count: "exact" });
+    adsCount.set(adsSell.length + adsRent.length);
 
-	supabase
-		.channel("")
-		.on(
-			"postgres_changes",
-			{ event: "*", schema: "public", table: "ads_sell" },
-			(payload) => {
-				ads.update((currentAds) => {
-					const newAd = { ...payload.new, section: "sales" };
-					const updatedAds = [newAd, ...currentAds];
-					adsCount.update((adsCount) => adsCount + 1);
-					return updatedAds;
-				});
-			}
-		)
-		.on(
-			"postgres_changes",
-			{ event: "*", schema: "public", table: "ads_rent" },
-			(payload) => {
-				ads.update((currentAds) => {
-					const newAd = { ...payload.new, section: "rental" };
-					const updatedAds = [newAd, ...currentAds];
-					adsCount.update((adsCount) => adsCount + 1);
-					return updatedAds;
-				});
-			}
-		)
-		.subscribe();
+    supabase
+        .channel("")
+        .on(
+            "postgres_changes",
+            { event: "*", schema: "public", table: "ads_sell" },
+            (payload) => {
+                if (firstLoadComplete) {
+                    showToast('Zaur zzaurował nowe ogłoszenia w dziale sprzedaży.');
+                } else {
+                    firstLoadComplete = true;
+                }
+                ads.update((currentAds) => {
+                    const newAd = { ...payload.new, section: "sales" };
+                    const updatedAds = [newAd, ...currentAds];
+                    adsCount.update((count) => count + 1);
+                    return updatedAds;
+                });
+            }
+        )
+        .on(
+            "postgres_changes",
+            { event: "*", schema: "public", table: "ads_rent" },
+            (payload) => {
+                if (firstLoadComplete) {
+                    showToast('Zaur zzaurował nowe ogłoszenia w dziale wynajmu.');
+                } else {
+                    firstLoadComplete = true;
+                }
+                ads.update((currentAds) => {
+                    const newAd = { ...payload.new, section: "rental" };
+                    const updatedAds = [newAd, ...currentAds];
+                    adsCount.update((count) => count + 1);
+                    return updatedAds;
+                });
+            }
+        )
+        .subscribe();
 }
--- src/routes/sections/Stats.svelte
+++ src/routes/sections/Stats.svelte
@@ -1,57 +1,74 @@
 <script lang="ts">
-  import { adsCount } from '$lib/adsCountStore';
-  import Button from '$lib/components/ui/button/button.svelte';
+	import { adsCount } from '$lib/adsCountStore';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import { onMount } from 'svelte';
-  import { SpringValue, format, timerStore } from 'svelte-ux';
-  let randomNumber =  Math.floor(Math.random() * 9999);
-  let numberOfAds : number = 0;
-  let numberOfZaurs : number = 0;
-  let numberOfRandom : number = 0;
+	import { SpringValue, format, timerStore } from 'svelte-ux';
+	let randomNumber = Math.floor(Math.random() * 9999);
+	let numberOfAds: number = 0;
+	let numberOfZaurs: number = 0;
+	let numberOfRandom: number = 0;
 
-  $: if ($adsCount > 0) {
-    numberOfAds += $adsCount;
-  }
+	$: if ($adsCount > 0) {
+		numberOfAds += $adsCount;
+	}
 
-  onMount(async () => {
-    numberOfZaurs += 1;
-    numberOfRandom += randomNumber;
+	onMount(async () => {
+		numberOfZaurs += 1;
+		numberOfRandom += randomNumber;
 	});
 </script>
 
-
 <!-- Features -->
-<div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto text-center">
-    <!-- Grid -->
-    <div class="mt-20 grid gap-6 grid-cols-2 sm:gap-12 lg:grid-cols-3 lg:gap-8">
-      <!-- Stats -->
-      <div>
-        <h4 data-static class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Number of Ads</h4>
-        <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-200">
-          <SpringValue value={$adsCount} format="integer" options={{ stiffness: 0.01, damping: 0.25 }}  />
-        </p>
-        <p data-static class="mt-1 text-gray-500">unique ads without ads</p>
-    </div>
-      <!-- End Stats -->
-  
-      <!-- Stats -->
-      <div>
-        <h4 data-static class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Number of Zaurs</h4>
-        <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-200">
-          <SpringValue value={numberOfZaurs} format="integer" options={{ stiffness: 0.01, damping: 0.25 }}  />
-        </p>
-        <p data-static class="mt-1 text-gray-500">powerful</p>
-      </div>
-      <!-- End Stats -->
-            <!-- Stats -->
-            <div>
-              <h4 data-static class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">A Random Number</h4>
-              <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-200">
-                <SpringValue value={numberOfRandom} format="integer" options={{ stiffness: 0.01, damping: 0.25 }}  />
-              </p>
-              <p data-static class="mt-1 text-gray-500">between 0 and 9999</p>
-            </div>
-            <!-- End Stats -->
-    </div>
-    <!-- End Grid -->
-  </div>
-  <!-- End Features -->
+<div class="mx-auto max-w-[85rem] px-4 text-center sm:px-6 lg:px-8">
+	<!-- Grid -->
+	<div class="mt-20 grid grid-cols-2 gap-6 sm:gap-12 lg:grid-cols-3 lg:gap-8">
+		<!-- Stats -->
+		<div>
+			<h4 data-static class="text-lg font-semibold text-gray-800 dark:text-gray-200 sm:text-xl">
+				Number of Ads
+			</h4>
+			<p class="mt-2 text-4xl font-bold text-blue-200 sm:mt-3 sm:text-6xl">
+				<SpringValue
+					value={$adsCount}
+					format="integer"
+					options={{ stiffness: 0.01, damping: 0.25 }}
+				/>
+			</p>
+			<p data-static class="mt-1 text-gray-500">unique ads without ads</p>
+		</div>
+		<!-- End Stats -->
+
+		<!-- Stats -->
+		<div>
+			<h4 data-static class="text-lg font-semibold text-gray-800 dark:text-gray-200 sm:text-xl">
+				Number of Zaurs
+			</h4>
+			<p class="mt-2 text-4xl font-bold text-blue-200 sm:mt-3 sm:text-6xl">
+				<SpringValue
+					value={numberOfZaurs}
+					format="integer"
+					options={{ stiffness: 0.01, damping: 0.25 }}
+				/>
+			</p>
+			<p data-static class="mt-1 text-gray-500">powerful</p>
+		</div>
+		<!-- End Stats -->
+		<!-- Stats -->
+		<div>
+			<h4 data-static class="text-lg font-semibold text-gray-800 dark:text-gray-200 sm:text-xl">
+				A Random Number
+			</h4>
+			<p class="mt-2 text-4xl font-bold text-blue-200 sm:mt-3 sm:text-6xl">
+				<SpringValue
+					value={numberOfRandom}
+					format="integer"
+					options={{ stiffness: 0.01, damping: 0.25 }}
+				/>
+			</p>
+			<p data-static class="mt-1 text-gray-500">between 0 and 9999</p>
+		</div>
+		<!-- End Stats -->
+	</div>
+	<!-- End Grid -->
+</div>
+<!-- End Features -->
