7e8040fa149b67d2532b69bdbaf284bc3cf0aed8
unknown <b.dymet@gmail.com>
2024-03-25 07:26:00 +0100

    Add hale-magazyny.

--- src/jobs/scrape-ads.ts
+++ src/jobs/scrape-ads.ts
@@ -3,52 +3,60 @@ import { client } from "../trigger";
 import { toast } from 'svelte-sonner';
 
 const services = ["olx", "nol", "gratka"];
-const types = ["mieszkania", "domy", "dzialki"];
+const allTypes = ["mieszkania", "domy", "dzialki", "hale-magazyny"];
 const categories = ["sprzedaz", "wynajem"];
 const adTypes = ["private", "business"];
 
 export const scrapeAds = client.defineJob({
-	id: "scrape-ads",
-	name: "Scrape Ads and Save to Database",
-	version: "0.0.1",
-	trigger: intervalTrigger({
-		seconds: 4000,
-	}),
+    id: "scrape-ads",
+    name: "Scrape Ads and Save to Database",
+    version: "0.0.4",
+    trigger: intervalTrigger({
+        seconds: 4000,
+    }),
 
-	run: async (payload, io, ctx) => {
-		toast.info('Zaur zauruje.');
-		const serviceIndex = Math.floor(Math.random() * services.length);
-		const typeIndex = Math.floor(Math.random() * types.length);
-		const categoryIndex = Math.floor(Math.random() * categories.length);
-		const adTypeIndex = Math.floor(Math.random() * adTypes.length);
-		const dynamicPayload = {
-			service: services[serviceIndex],
-			type: types[typeIndex],
-			category: categories[categoryIndex],
-			adType: adTypes[adTypeIndex],
-		};
-		const endpointUrl = `http://109.123.250.191:5050/fetch-ads/${dynamicPayload.service}/${dynamicPayload.type}/${dynamicPayload.category}/${dynamicPayload.adType}`;
-		const content = await io.backgroundFetch(
-			"fetch-content",
-			endpointUrl,
-			{
-				method: "GET",
-			},
-			{},
-			{
-				durationInMs: 40000,
-				retry: {
-					limit: 6,
-					minTimeoutInMs: 1000,
-					maxTimeoutInMs: 80000,
-					factor: 2
-				},
-			}
-		);
+    run: async (payload, io, ctx) => {
+        const serviceIndex = Math.floor(Math.random() * services.length);
+        const selectedService = services[serviceIndex];
+        let types = allTypes;
+        if (selectedService === "gratka") {
+            types = allTypes.filter(type => type !== "hale-magazyny");
+        }
 
-		return {
-			message: "Content fetched successfully with random parameters.",
-			content,
-		};
-	},
+        const typeIndex = Math.floor(Math.random() * types.length);
+        const categoryIndex = Math.floor(Math.random() * categories.length);
+        const adTypeIndex = Math.floor(Math.random() * adTypes.length);
+        
+        const dynamicPayload = {
+            service: selectedService,
+            type: types[typeIndex],
+            category: categories[categoryIndex],
+            adType: adTypes[adTypeIndex],
+        };
+
+        const endpointUrl = `http://109.123.250.191:5050/fetch-ads/${dynamicPayload.service}/${dynamicPayload.type}/${dynamicPayload.category}/${dynamicPayload.adType}`;
+        
+        const content = await io.backgroundFetch(
+            "fetch-content",
+            endpointUrl,
+            {
+                method: "GET",
+            },
+            {},
+            {
+                durationInMs: 25000,
+                retry: {
+                    limit: 6,
+                    minTimeoutInMs: 1000,
+                    maxTimeoutInMs: 80000,
+                    factor: 2
+                },
+            }
+        );
+
+        return {
+            message: "Content fetched successfully with random parameters.",
+            content,
+        };
+    },
 });
